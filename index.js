b2a7b552000f54fdbc37f2dd5a951fb2164537b4
jenniekibiri <josphatje@gmail.com>
2020-05-30 08:56:19 -0400

    add morgan

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const morgan = require('morgan')
 const app= express();
-
+app.use(morgan('dev'))
 const {getPosts}=require('./routes/posts')
 app.get('/',getPosts)
 const port = 8080;
