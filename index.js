aaf89d436944df12859eb48d6dd8edc1f5c3cd5b
jenniekibiri <josphatje@gmail.com>
2020-05-30 05:07:17 -0400

    seperate routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app= express();
-app.get('/home',(req,res)=>{
-    res.send('this is a test route that definately working')
-})
+
+const {getPosts}=require('./routes/posts')
+app.get('/',getPosts)
 const port = 8080;
 app.listen(port,()=>{
     console.log(`the  server is running on port ${port}`)
