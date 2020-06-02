7a2d96e604b95743946aa742a42cb4d03f0365eb
jenniekibiri <josphatje@gmail.com>
2020-06-02 10:12:51 -0400

    add user signing vaalidation

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router()
 const {getPosts,getAllPosts,deletePosts,updatePost,createPosts} = require('../controllers/post')
 const {signup}= require('../controllers/users')
-const {postValidator} = require('../validator')
+const {postValidator,signinValidator} = require('../validator')
 
 
 router.get('/',getPosts)
 router.post('/posts',postValidator,createPosts)
-router.post('/signup',signup)
+router.post('/signup',signinValidator,signup)
 router.get('/allposts',getAllPosts)
 router.delete('/deletepost',deletePosts)
 router.put('/updatepost',updatePost)
--- validator/index.js
+++ validator/index.js
@@ -21,3 +21,29 @@ exports.postValidator =(req,res,next)=>{
    }
    next();
 }
+exports.signinValidator =(req,res,next)=>{
+    req.check('name','name is required').notEmpty();
+    req.check('password','password cannot be empty').notEmpty();
+    req.check('email','email must be between 3 to 32 characters')
+    .matches(/.+\@.+\..+/)
+    .withMessage('Emailmust contain @')
+    .isLength({
+        min:3,
+        max:32
+    })
+req.check('password').isLength({
+    min:8
+})
+.withMessage('password must contain 8 characters')
+.matches(/\d/)
+.withMessage('password must contain a number');
+const errors = req.validationErrors();
+//show errors as they appear
+if(errors){
+    const firstError = errors.map((error)=>error.msg)[0]
+    return res.status(400).json({
+        error:firstError
+    })
+}
+next();
+}
