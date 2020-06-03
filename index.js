7e28c6a4500d9c18ed92284c9f217694e7ab44d4
jenniekibiri <josphatje@gmail.com>
2020-06-03 02:07:53 -0400

    add cookie parser

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ dotenv.config();
 const bodyParser = require('body-parser')
 const expressValidator= require('express-validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const cookieParser= require('cookie-parser')
 
 
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true,  useUnifiedTopology: true } ).then(()=>console.log('database connected'))
@@ -14,7 +16,7 @@ const app= express();
 app.use(expressValidator())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
-
+app.use(cookieParser())
 const postRoutes = require('./routes/posts')
 app.use('/',postRoutes)
 app.use('/posts',postRoutes)
