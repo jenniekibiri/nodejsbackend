1baeba8ad337f6481f6dbf46ed7c592531bd0cc4
jenniekibiri <josphatje@gmail.com>
2020-06-08 05:37:54 -0400

    add user post relationship

--- model/post.js
+++ model/post.js
@@ -8,6 +8,15 @@ required:true
      body:{
 type:String,
 required:true
+     },
+     postedBy:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User'  
+     },
+     created:{
+          type:Date,
+          default:Date.now
+
      }
  })
  module.exports = mongoose.model('Posts',postSchema)
