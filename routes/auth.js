b8bff4d29986d6b2122cd42922dd99132ba031e8
jenniekibiri <josphatje@gmail.com>
2020-08-20 01:03:38 -0400

    add social login

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-const { signin, signout, signup } = require("../controllers/auth");
+const { signin, signout, signup,socialLogin} = require("../controllers/auth");
 const { userById } = require("../controllers/users");
 
 const { signinValidator } = require("../validator");
-
+router.post("/sociallogin", socialLogin)
 router.post("/signup", signinValidator, signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
